0f1010a265dfb52cfc26c6edbe64701c6d30893d
martendo <51766598+martendo@users.noreply.github.com>
2023-05-18 14:55:51 -0400

    Add rudimentary shaker instrument

--- Muvica/ShakerView.swift
+++ Muvica/ShakerView.swift
@@ -1,14 +1,34 @@
 import SwiftUI
+import CoreMotion
 
 struct ShakerView: View {
+	let shakeThreshold = 1.0
+
 	@ObservedObject private var motionDetector = MotionDetector.shared
+
+	@State private var wasShaking: Bool = false
 
 	var body: some View {
 		List {
 			Text("Shaker")
 		}
 		.onAppear {
-			motionDetector.callback = nil
+			motionDetector.callback = handleMotion(data:)
 		}
+	}
+
+	func handleMotion(data: CMDeviceMotion) {
+		// Detect shake from magnitude of acceleration
+		let a = data.userAcceleration
+		let magsqr = a.x * a.x + a.y * a.y + a.z * a.z
+		let isShaking = magsqr >= shakeThreshold
+		if isShaking && !wasShaking {
+			doShake()
+		}
+		wasShaking = isShaking
+	}
+
+	func doShake() {
+		print("Shook!")
 	}
 }
