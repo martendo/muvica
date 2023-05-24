a5d55f50537909c7f1cee6a04878c1b6b2737331
martendo <51766598+martendo@users.noreply.github.com>
2023-05-24 14:43:36 -0400

    Use withAnimation for more consistent shaker animations

--- Muvica/ShakerRingView.swift
+++ Muvica/ShakerRingView.swift
@@ -3,20 +3,35 @@ import SwiftUI
 struct ShakerRingView: View {
 	let ringWidth: Double
 	let deviceShakeValue: Double
-	var isShaking: Bool {
-		return deviceShakeValue >= 1.0
-	}
-	var circleWidth: Double {
-		return ringWidth * min(deviceShakeValue, 1)
-	}
+
+	@State private var isCircleColored: Bool = false
+	@State private var circleWidth: Double = 0
 
     var body: some View {
 		ZStack {
 			Circle()
-				.foregroundColor(isShaking ? .red.opacity(0.5) : .accentColor.opacity(0.5))
-				.animation(isShaking ? nil : .easeOut, value: isShaking)
+				.foregroundColor(isCircleColored ? .red.opacity(0.5) : .accentColor.opacity(0.5))
 				.frame(width: circleWidth, height: circleWidth)
-				.animation(.interactiveSpring(blendDuration: 0.15), value: circleWidth)
+				.onChange(of: deviceShakeValue >= 1.0) { newValue in
+					if !newValue {
+						withAnimation(.easeOut) {
+							isCircleColored = false
+						}
+					} else {
+						// No animation
+						isCircleColored = true
+					}
+				}
+				.onChange(of: deviceShakeValue) { newValue in
+					if newValue < 1.0 {
+						withAnimation(.interactiveSpring(blendDuration: 0.15)) {
+							circleWidth = ringWidth * newValue
+						}
+					} else {
+						// No animation and cap at ringWidth
+						circleWidth = ringWidth
+					}
+				}
 			Circle()
 				.stroke(lineWidth: 3)
 				.foregroundColor(.accentColor.opacity(0.8))
