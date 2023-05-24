d52c04aef14bab302a91bce67b518e7057bd4706
martendo <51766598+martendo@users.noreply.github.com>
2023-05-24 13:47:38 -0400

    Cap shaker circle size

--- Muvica/ShakerRingView.swift
+++ Muvica/ShakerRingView.swift
@@ -6,18 +6,21 @@ struct ShakerRingView: View {
 	var isShaking: Bool {
 		return deviceShakeValue >= 1.0
 	}
+	var circleWidth: Double {
+		return ringWidth * min(deviceShakeValue, 1)
+	}
 
     var body: some View {
 		ZStack {
 			Circle()
 				.foregroundColor(isShaking ? .red.opacity(0.5) : .accentColor.opacity(0.5))
 				.animation(isShaking ? nil : .easeOut, value: isShaking)
-				.frame(width: ringWidth * deviceShakeValue, height: ringWidth * deviceShakeValue)
-				.animation(.interactiveSpring(blendDuration: 0.15), value: deviceShakeValue)
+				.frame(width: circleWidth, height: circleWidth)
+				.animation(.interactiveSpring(blendDuration: 0.15), value: circleWidth)
 			Circle()
 				.stroke(lineWidth: 3)
 				.foregroundColor(.accentColor.opacity(0.8))
 				.frame(width: ringWidth, height: ringWidth)
 		}
-    }
+	}
 }
